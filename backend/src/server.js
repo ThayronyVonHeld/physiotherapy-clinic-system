3f36eb382d4af12d60e4c4d3b160476c3a229d68
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-03-17 15:19:02 -0300

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import dotenv from "dotenv";
 dotenv.config();
-
 import connectDB from "./config/db.js";
-
 connectDB();
-dotenv.config();
 
 const app = express();
 
